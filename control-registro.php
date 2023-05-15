c515d2ed4f5365e128d9596c972d421c1a015454
ezef8137 <ezef8137@gmail.com>
2023-05-15 19:05:19 -0300

    Guardando cambios antes de cambiar de rama

--- control-registro.php
+++ control-registro.php
@@ -7,29 +7,61 @@ if ($con){
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $fecha_nacimiento = mysqli_real_escape_string($con,$_POST['fecha_nacimiento']);
+    $fecha_nacimiento = date('Y-m-d',strtotime(mysqli_real_escape_string($con,$_POST['fecha_nacimiento'])));
     $direccion = mysqli_real_escape_string($con,$_POST['direccion']);
     $telefono = mysqli_real_escape_string($con,$_POST['telefono']);
-    $correo = mysqli_real_escape_string($con,$_POST['email']);
+    $correo = mysqli_real_escape_string($con,$_POST['correo']);
     $nombre = mysqli_real_escape_string($con,$_POST['nombre']);
     $apellido = mysqli_real_escape_string($con,$_POST['apellido']);
     $dni = mysqli_real_escape_string($con,$_POST['dni']);
+    $password = mysqli_real_escape_string($con,$_POST['password']);
     }
 
-if (isset($_POST["registro"])){
-    $consulta="INSERT INTO personas (id_usuario,fecha_nacimiento,direccion,telefono,correo,nombre,apellido,dni) VALUES ('ABC122','$fecha_nacimiento','$direccion','$telefono',$correo,$nombre,$apellido,$dni)";
-    $resultado = mysqli_query($con,$consulta);
-	    if ($resultado) {
-	    	?> 
-	    	<h3 class="ok">¡Te has inscripto correctamente!</h3>
-           <?php
-	    } else {
-	    	?> 
-	    	<h3 class="bad">¡Ups ha ocurrido un error!</h3>
-           <?php
-	    }
-    }   else {
-	    	?> 
-	    	<h3 class="bad">¡Por favor complete los campos!</h3>
-           <?php
+
+    if (isset($_POST["registro"])) {
+        $correo = $_POST["correo"];
+        $dni = $_POST["dni"];
+    
+        // Validar que correo y DNI no existan en la base de datos
+        $validar = "SELECT * FROM personas WHERE correo='$correo' OR dni='$dni'";
+        $result = mysqli_query($con, $validar);
+    
+        if (mysqli_num_rows($result) > 0) {
+            // El usuario ya existe
+            ?>
+            <h3 class="na">¡El usuario ya existe en la base de datos!</h3>
+            <?php
+        } else {
+            // Insertar nuevo usuario
+            $fecha_nacimiento = $_POST["fecha_nacimiento"];
+            $direccion = $_POST["direccion"];
+            $telefono = $_POST["telefono"];
+            $nombre = $_POST["nombre"];
+            $apellido = $_POST["apellido"];
+    
+            $consulta1 = "INSERT INTO personas (fecha_nacimiento, direccion, telefono, correo, nombre, apellido, dni) VALUES ('$fecha_nacimiento', '$direccion', '$telefono', '$correo', '$nombre', '$apellido', '$dni')";
+            $resultado1 = mysqli_query($con, $consulta1);
+            $personas_id_usuario= mysqli_insert_id($con);
+            $consulta2= "INSERT INTO usuario_cliente(personas_id_usuario,password) VALUES ('$personas_id_usuario','$password')";
+            $resultado2 = mysqli_query($con,$consulta2);
+            
+    
+            if ($resultado1 and $resultado2) {
+                // Usuario registrado correctamente
+                header("Location: index.php"); // Redirigir a la página de inicio
+                exit();
+            } else {
+                // Error al registrar el usuario
+                ?>
+                <h3 class="bad">¡ha ocurrido un error!</h3>
+                <?php
+            }
+        }
+    } else {
+        // Formulario no enviado
+        ?>
+        <h3 class="bad">¡Por favor complete los campos!</h3>
+        <?php
     }
+    
+    
--- registro.php
+++ registro.php
@@ -1,23 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/registro.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <script src="https://kit.fontawesome.com/8c68749bc1.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Abril+Fatface" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/registro.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <script src="https://kit.fontawesome.com/8c68749bc1.js" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+    crossorigin="anonymous"></script>
+  <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Abril+Fatface" rel="stylesheet">
 
-    <title>Document</title>
+  <title>Document</title>
 </head>
+
 <body>
   <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
     <div class="container">
       <a class="navbar-brand" href="index.php">Travel Dreams</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav"
+        aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="oi oi-menu"></span> Menu
       </button>
 
@@ -35,33 +40,34 @@
     </div>
   </nav>
   <div class="regis-box"></div>
-    <form class="form" action="" method="POST">
-        <h1 class="title">Registro</h1>
-        <span class="sub mb"> Ingrese sus Datos</span>
-        <input id="file" type="file">
-        <i class="fa-solid fa-person-circle-plus fa-bounce fa-2xl" style="color: #4b37e1;" id="logo"></i>
-  
-        <div name="datos">
-          <input type="email" class="input" placeholder="email" name="email" >
-          <input type="password" class="input" placeholder="password" name="password"> 
-          <input type="text" class="input" placeholder="Nombre" name="nombre" >
-          <input type="text" class="input" placeholder="Apellido" name="apellido">
-          <input type="int" class="input" placeholder="DNI" name="dni">
-          <input type="text" class="input" placeholder="Dirección" name="direccion">
-          <input type="date" class="input" placeholder="Fecha Nacimiento" name="fecha_nacimiento">
-          <input type="tel" class="input" placeholder="Nro Teléfono" name="telefono">
-        </div>
-        <div class="suscrip">
-          <span class="sub">¿Ya Tienes Una Cuenta? <a href="login.php">Ingresa</a></span>
-          <button type="submit" value="registro" class="btn btn-outline-secondary" name="registro">Registrarme</button>
-        </div>
-       
+  <form class="form" action="" method="POST">
+    <h1 class="title">Registro</h1>
+    <span class="sub mb"> Ingrese sus Datos</span>
+    <input id="file" type="file">
+    <i class="fa-solid fa-person-circle-plus fa-bounce fa-2xl" style="color: #4b37e1;" id="logo"></i>
 
-    </form>
-    <?php
-      include ("control-registro.php");
-    ?>
-    
+    <div name="datos">
+      <input type="email" class="input" placeholder="email" name="correo">
+      <input type="password" class="input" placeholder="password" name="password">
+      <input type="text" class="input" placeholder="Nombre" name="nombre">
+      <input type="text" class="input" placeholder="Apellido" name="apellido">
+      <input type="int" class="input" placeholder="DNI" name="dni">
+      <input type="text" class="input" placeholder="Dirección" name="direccion">
+      <input type="date" class="input" placeholder="Fecha Nacimiento" name="fecha_nacimiento">
+      <input type="tel" class="input" placeholder="Nro Teléfono" name="telefono">
+    </div>
+    <div class="suscrip">
+      <span class="sub">¿Ya Tienes Una Cuenta? <a href="login.php">Ingresa</a></span>
+      <button type="submit" value="registro" class="btn btn-outline-secondary" name="registro">Registrarme</button>
+    </div>
+
+
+  </form>
+  <?php
+  include("control-registro.php");
+  ?>
+
 
 </body>
+
 </html>
